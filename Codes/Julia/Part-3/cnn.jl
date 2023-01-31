c23dfe25d2a69e5a46063130292b8e864ddb51fb
a-mhamdi <a_mhamdi@outlook.com>
2023-01-31 02:00:56 +0100

    MNIST handwritten digit classifier

--- Codes/Julia/Part-3/cnn.jl
+++ Codes/Julia/Part-3/cnn.jl
@@ -1,46 +1,110 @@
-#= CNN =#
+##############################################
+#= HANDWRITTEN DIGITS RECOGNITION USING CNN =#
+##############################################
 
-# Here is a simple example of how a CNN could be implemented in Julia using the Flux.jl library:
+using Markdown
+md"Handwritten digits classification using **CNN**. This solution is implemented in `Julia` using the `Flux.jl` library"
 
+using Statistics
+using ProgressMeter
+using Plots
+
+md"Import the machine learning library `Flux`" 
 using Flux
+using Flux: Data.DataLoader
+using Flux: onecold, onehotbatch
 
-# define the CNN
-function cnn(x)
-    # the input x is a batch of images with dimensions (batch_size, height, width, channels)
-    # define the convolutional layers
-    c1 = Conv((3, 3), 3=>8, pad=(1, 1))(x)
-    c2 = Conv((3, 3), 8=>16, pad=(1, 1))(c1)
-    # define the pooling layers
-    p1 = MaxPool((2, 2))(c2)
-    p2 = MaxPool((2, 2))(p1)
-    # flatten the output of the pooling layers
-    f = flatten(p2)
-    # define the fully connected layers
-    fc1 = Dense(128, 32)(f)
-    fc2 = Dense(32, 10)(fc1)
-    # return the output of the fully connected layers
-    return fc2
+using MLDatasets
+d = MNIST()
+
+Base.@kwdef mutable struct Args
+    η = 3e-3                # Learning rate
+    batchsize = 64          # Batch size
+    epochs = 8             # Number of epochs
+    split = :train          # Split data into `train` and `test`
 end
 
-# define the loss function
-loss(ŷ, y) = Flux.mse(ŷ, y)
-
-# define the accuracy metric
-accuracy(ŷ, y) = mean(Flux.argmax(ŷ) .== Flux.argmax(y))
-
-# train the CNN
-for (x, y) in data
-    # pass the input data through the CNN to get the prediction
-    ŷ = cnn(x)
-    # compute the loss
-    l = loss(ŷ, y)
-    # update the network weights using gradient descent
-    Flux.back!(l)
-    # compute the accuracy
-    a = accuracy(ŷ, y)
-    # print the loss and accuracy
-    println("Loss: $(l), Accuracy: $(a)")
+md"Load the **MNIST** dataset"
+function get_data(; kws...)
+    args = Args(; kws...);
+    data = MNIST(split=args.split);
+    md"Split and normalize data"
+    X, y = data.features ./ 255, data.targets;
+    X = reshape(X, (28, 28, 1, :));
+    y = onehotbatch(y, 0:9);
+    loader = DataLoader((X, y); batchsize=args.batchsize, shuffle=true);
+    return loader
 end
 
+train_loader = get_data();
+test_loader = get_data(split=:test);
 
-# This is just a rough example, and there are many details that have been left out (such as how to load and preprocess the data, how to use a validation set to evaluate the model, etc.). However, this should give you an idea of how a CNN can be implemented in Julia using the Flux.jl library.
+md"Transform sample training data to an image. View the image and check the corresponding digit value."
+using ImageShow, ImageInTerminal
+idx = rand(1:6000);
+convert2image(d, idx)
+md"**Digit is $(d.targets[idx])**"
+
+md"""
+## **CNN** ARCHITECTURE"
+The input `X` is a batch of images with dimensions `(width=28, height=28, channels=1, batchsize)`
+"""
+
+fc = prod(Int.(floor.([28/4 - 2, 28/4 - 2, 16]))) # 2^{\# max-pool}
+model = Chain(
+            Conv((5, 5), 1 => 16, relu),
+            MaxPool((2, 2)),
+            Conv((3, 3), 16 => 16, relu),
+            MaxPool((2, 2)),
+            Flux.flatten,
+            Dense(fc => 64, relu),
+            Dense(64 => 32, relu),
+            Dense(32 => 10),
+            softmax
+)
+
+function train(; kws...)
+    args = Args(; kws...)
+    md"Define the loss function"
+    l(α, β) = Flux.logitcrossentropy(α, β)
+    md"Define the accuracy metric"
+    acc(α, β) = mean(onecold(α) .== onecold(β))
+    md"Optimizer"
+    optim_state = Flux.setup(Adam(args.η), model);
+
+    vec_loss = []
+    vec_acc = []
+
+    for epoch in 1:args.epochs
+        printstyled("\t***\t === EPOCH $(epoch) === \t*** \n", color=:magenta, bold=true)
+        @info "TRAINING"
+        @showprogress for (X, y) in train_loader
+            loss, grads = Flux.withgradient(model) do m
+                ŷ = m(X);
+                l(ŷ, y);
+            end
+            Flux.update!(optim_state, model, grads[1]); # Upd `W` and `b`
+        end
+        @info "TESTING"
+        @showprogress for (X, y) in test_loader
+            ŷ = model(X);
+            push!(vec_loss, l(ŷ, y));  # log `loss` value -> `vec_loss` vector
+            push!(vec_acc, acc(ŷ, y)); # log `acc` value -> `vec_acc` vector
+        end
+    end
+    return vec_loss, vec_acc     
+end
+
+vec_loss, vec_acc = train()
+
+# Plot results
+plot(vec_loss, label="Training Loss")
+plot(vec_acc, label="Test Accuracy")
+
+# Let's make some predictions
+idx = rand(1:1000, 16)
+xs, ys = test_loader.data[1][:,:,:,idx], onecold(test_loader.data[2][:, idx]) .- 1
+yp = onecold(model(xs)) .- 1
+for i in 1:length(yp)
+    @info  "**Predicted is $(yp[i]). Label is $(ys[i]).**"
+end
