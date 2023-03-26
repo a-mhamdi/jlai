34b0edd00f471c682724aa8034b8bcc0bc35090a
a-mhamdi <a_mhamdi@outlook.com>
2023-03-26 18:27:21 +0100

    change index

--- Codes/Julia/Part-2/gradient-descent.jl
+++ Codes/Julia/Part-2/gradient-descent.jl
@@ -22,11 +22,11 @@ md"*Stocastic Gradient Descent (SGD)*"
 α, n, θ = 0.0003, 5, [10; .5]
 J = []
 for _ in 1:1_000 # RUN IT MULTIPLE TIMES TO CONVERGE
-    for i in shuffle(1:5)
-		cost = ( y[i] - X[i, :]' * θ )^2
+    for k in shuffle(1:5)
+		cost = ( y[k] - X[k, :]' * θ )^2
 		push!(J, cost);
-        θ += α * (y[i] - X[i, :]' * θ) * X[i, :]
-        println("θ is $(θ)")
+		θ += α * (y[k] - X[k, :]' * θ) * X[k, :]
+		println("θ is $(θ)")
     end
 end
 
@@ -36,11 +36,11 @@ md"*Batch Gradient Descent*"
 α, n, θ = 0.0003, 5, [10; .5]
 J = []
 for _ in 1:1_000
-    ϵ = (y-X*θ)[:, 1]
-    cost = (2*n)\ ϵ'*ϵ
+	ϵ = (y-X*θ)[:, 1]
+	cost = (2*n)\ ϵ'*ϵ
 	push!(J, cost)
-    θ += n\α * sum((y - X * θ) .* X, dims=1)'
-    println("θ is $(θ)")
+	θ += n\α * sum((y - X * θ) .* X, dims=1)'
+	println("θ is $(θ)")
 end
 
 plot(J)
