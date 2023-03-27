cb8c1365d4719544430c05439226dceb74f8aea0
a-mhamdi <a_mhamdi@outlook.com>
2023-03-28 00:13:01 +0100

    multivariable regression

--- Codes/Julia/Part-2/multivariable-regression.jl
+++ Codes/Julia/Part-2/multivariable-regression.jl
@@ -30,7 +30,7 @@ train, test = partition(eachindex(y), 0.8, shuffle=true, rng=123)
 Xtrain, Xtest = X[train, :], X[test, :]
 ytrain, ytest = y[train], y[test]
 
-md"Load & Instantiate The Linear Regressor Class"
+md"Load & Instantiate The Linear Regressor Model"
 LR = @load LinearRegressor pkg=MLJLinearModels
 lr_ = LR()
 
@@ -39,7 +39,40 @@ lr = machine(lr_, Xtrain, ytrain) |> fit!
 println("Params of fitted model are $(fitted_params(lr))")
 
 md"Prediction"
-yhat = predict(lr, Xtest)
+yhat_lr = predict(lr, Xtest)
 
 md"Results & Metrics"
-println("Error is $(sum((yhat .- ytest).^2) ./ length(ytest))")
+println("Error is $(sum((yhat_lr .- ytest).^2) ./ length(ytest))")
+
+md"Using `MLJ` Builtin Methods For Evaluation"
+MLJ.evaluate!(lr, measure=[l1, l2, rms])
+
+### RIDGE REGRESSOR
+md"Load Ridge Regressor"
+RIDGE = @load RidgeRegressor pkg=MLJLinearModels
+ridge_= RIDGE()
+md"Train & Fit The Regressor"
+ridge = machine(ridge_, Xtrain, ytrain) |> fit!
+md"Evalute The Model"
+yhat_ridge = predict(ridge, Xtest)
+println("Error is $(sum((yhat_ridge .- ytest).^2) ./ length(ytest))")
+
+### LASSO REGRESSOR
+md"Load Lasso Regressor"
+LASSO = @load LassoRegressor pkg=MLJLinearModels
+lasso_= LASSO()
+md"Train & Fit The Regressor"
+lasso = machine(lasso_, Xtrain, ytrain) |> fit!
+md"Evalute The Model"
+yhat_lasso = predict(lasso, Xtest)
+println("Error is $(sum((yhat_lasso .- ytest).^2) ./ length(ytest))")
+
+### ELASTIC NET REGRESSOR
+md"Load Elastic Net Regressor"
+EN = @load ElasticNetRegressor pkg=MLJLinearModels
+en_= EN(lambda=.2)
+md"Train & Fit The Regressor"
+en = machine(en_, Xtrain, ytrain) |> fit!
+md"Evalute The Model"
+yhat_en = predict(en, Xtest)
+println("Error is $(sum((yhat_en .- ytest).^2) ./ length(ytest))")
