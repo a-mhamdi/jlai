3db93b1eb82626708ab3284700716e6e01ef88b8
a-mhamdi <a_mhamdi@outlook.com>
2023-04-10 00:37:01 +0100

    k-nearest neighbors

--- Codes/Julia/Part-2/knn.jl
+++ Codes/Julia/Part-2/knn.jl
@@ -14,24 +14,37 @@ df = CSV.read("../Datasets/Social_Network_Ads.csv", DataFrame)
 
 md"Unpack Data"
 features, target = unpack(df,
-                          ==(:EstimatedSalary),
-                          ==(:Purchased);
-                          :EstimatedSalary => Continuous,
-                          :Purchased => Multiclass)
+                            ==(:EstimatedSalary),
+                            ==(:Purchased);
+                            :EstimatedSalary => Continuous,
+                            :Purchased => Multiclass)
 
 md"Scatter Plot"
 scatter(features, target; group=target, legend=false)
 
-md"Convert Data"
-x = Tables.table(features);
-y = target;
+md"Split The Data Into Train & Test Sets"
+train, test = partition(eachindex(target), 0.8, rng=123)
+xtrain, xtest = table(features[train, :]), table(features[test, :])
+ytrain, ytest = target[train], target[test]
 
 md"Find Nearest Neighbors"
 KNN = @load KNNClassifier pkg=NearestNeighborModels
 knn_ = KNN(K=3)
-knn = machine(knn_, x, y) |> fit!
+knn = machine(knn_, xtrain, ytrain) |> fit!
 
 md"You may want to see [NearestNeighborModels.jl](https://github.com/JuliaAI/NearestNeighborModels.jl) and the unwrapped model type [`NearestNeighborModels.KNNClassifier`](@ref)."
 
-md"Evaluate Model"
+md"Make predictions on `xtest`"
+yhat = predict_mode(knn, xtest)
+
+md"Confusion Matrix"
+confusion_matrix(yhat, ytest)
+
+md"Evaluation the model's performances"
+accuracy(yhat, ytest)
+precision(yhat, ytest)
+recall(yhat, ytest)
+f1score(yhat, ytest)
+
+md"Estimate the performance of `knn`"
 evaluate!(knn)
