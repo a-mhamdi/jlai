eb28a36b585fc25b4ca25cbd264eabb0422dae4f
a-mhamdi <a_mhamdi@outlook.com>
2023-04-10 11:41:08 +0100

    polynomial regression

--- Codes/Julia/Part-2/polynomial-regression.jl
+++ Codes/Julia/Part-2/polynomial-regression.jl
@@ -3,12 +3,12 @@
 ##################################
 using Markdown
 
-md"Import Librairies"
+md"Import librairies"
 using CSV, DataFrames
 using Plots
 using MLJ
 
-md"Read Data From File"
+md"Read data from file"
 df = CSV.read("../Datasets/Position_Salaries.csv", DataFrame)
 schema(df)
 
@@ -18,16 +18,16 @@ n = 4
 X = zeros(size(x)[1], n) # length(x.Level)
 y = df.Salary
 
-md"Partition Of Data"
+md"Partition of data"
 train, test = partition(eachindex(y), 0.8, shuffle=true, rng=123)
 Xtrain, Xtest = X[train, :], X[test, :]
 ytrain, ytest = y[train], y[test]
 
-md"Load Linear Regressor"
+md"Load linear regression model"
 LR = @load LinearRegressor pkg=MLJLinearModels
 lr_ = LR()
 
-md"Add Extra Features & Scaling"
+md"Add extra features & scaling"
 sc_ = Standardizer()
 for i in 1:n
     sc = machine(sc_, x.Level[train].^i) |> fit!
@@ -35,15 +35,15 @@ for i in 1:n
     Xtest[:, i] = MLJ.transform(sc, x.Level[test].^i)
 end
 
-md"Train & Fit The Regressor"
+md"Train & fit the regression model"
 lr = machine(lr_, table(Xtrain), ytrain) |> fit!
 params = fitted_params(lr)
 
-md"Predict & Measure The Error"
+md"Predict & measure the error"
 yhat = predict(lr, table(Xtest))
 println("Error is $(sum(( yhat.- ytest ).^2) ./ length(ytest) )")
 
-md"Plot & Compare"
+md"Plot & compare"
 using LaTeXStrings
 plot(x.Level[test], ytest, label=L"y(t)")
 plot!(x.Level[test], yhat, label=L"\hat{y}(t)")
