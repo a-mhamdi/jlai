dc00aa9b7a58f06cbf63f318be19416ce2583c08
a-mhamdi <a_mhamdi@outlook.com>
2023-04-10 11:40:41 +0100

    multivariable regression

--- Codes/Julia/Part-2/multivariable-regression.jl
+++ Codes/Julia/Part-2/multivariable-regression.jl
@@ -4,46 +4,46 @@
 
 using Markdown
 
-md"Import Librairies"
+md"Import librairies"
 using CSV, DataFrames
 using MLJ
 
-md"Load Data From CSV File"
+md"Load data from CSV file"
 df = CSV.read("../Datasets/50_Startups.csv", DataFrame)
 schema(df)
 
-md"Design The Features"
+md"Design the features"
 X = df[!, 1:4]
 colnames = ["rd", "admin", "spend", "state"]
 rename!(X, Symbol.(colnames))
 coerce!(X, :state => Multiclass)
 
-md"Encoding The State Column"
+md"Encoding the state column"
 ce = ContinuousEncoder()
 X = machine(ce, X) |> fit! |> MLJ.transform
 
-md"Extract Target Vector"
+md"Extract target vector"
 y = df.Profit
 
-md"Preparing For The Split"
+md"Preparing for the split"
 train, test = partition(eachindex(y), 0.8, shuffle=true, rng=123)
 Xtrain, Xtest = X[train, :], X[test, :]
 ytrain, ytest = y[train], y[test]
 
-md"Load & Instantiate The Linear Regressor Model"
+md"Load & instantiate the linear regression model"
 LR = @load LinearRegressor pkg=MLJLinearModels
 lr_ = LR()
 
 md"You may want to see [MLJLinearModels.jl](https://github.com/JuliaAI/MLJLinearModels.jl) and the unwrapped model type [`MLJLinearModels.LinearRegressor`](@ref)."
 
-md"Train & Fit"
+md"Train & fit"
 lr = machine(lr_, Xtrain, ytrain) |> fit!
 println("Params of fitted model are $(fitted_params(lr))")
 
 md"Prediction"
 yhat_lr = predict(lr, Xtest)
 
-md"Results & Metrics"
+md"Results & metrics"
 println("Error is $(sum((yhat_lr .- ytest).^2) ./ length(ytest))")
 
 md"Using `MLJ` Builtin Methods For Evaluation"
@@ -53,9 +53,9 @@ MLJ.evaluate!(lr, measure=[l1, l2, rms])
 md"Load Ridge Regressor"
 RIDGE = @load RidgeRegressor pkg=MLJLinearModels
 ridge_= RIDGE()
-md"Train & Fit The Regressor"
+md"Train & fit the model"
 ridge = machine(ridge_, Xtrain, ytrain) |> fit!
-md"Evalute The Model"
+md"Evalute the model"
 yhat_ridge = predict(ridge, Xtest)
 println("Error is $(sum((yhat_ridge .- ytest).^2) ./ length(ytest))")
 
@@ -63,9 +63,9 @@ println("Error is $(sum((yhat_ridge .- ytest).^2) ./ length(ytest))")
 md"Load Lasso Regressor"
 LASSO = @load LassoRegressor pkg=MLJLinearModels
 lasso_= LASSO()
-md"Train & Fit The Regressor"
+md"Train & fit the model"
 lasso = machine(lasso_, Xtrain, ytrain) |> fit!
-md"Evalute The Model"
+md"Evalute the model"
 yhat_lasso = predict(lasso, Xtest)
 println("Error is $(sum((yhat_lasso .- ytest).^2) ./ length(ytest))")
 
@@ -73,8 +73,8 @@ println("Error is $(sum((yhat_lasso .- ytest).^2) ./ length(ytest))")
 md"Load Elastic Net Regressor"
 EN = @load ElasticNetRegressor pkg=MLJLinearModels
 en_= EN(lambda=.2)
-md"Train & Fit The Regressor"
+md"Train & fit the model"
 en = machine(en_, Xtrain, ytrain) |> fit!
-md"Evalute The Model"
+md"Evalute the model"
 yhat_en = predict(en, Xtest)
 println("Error is $(sum((yhat_en .- ytest).^2) ./ length(ytest))")
