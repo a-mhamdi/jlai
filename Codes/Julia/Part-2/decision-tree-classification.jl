7ce82e621653f6416288dbdf060b45983e582375
a-mhamdi <a_mhamdi@outlook.com>
2023-04-10 11:37:55 +0100

    decision tree classifier

--- Codes/Julia/Part-2/decision-tree-classification.jl
+++ Codes/Julia/Part-2/decision-tree-classification.jl
@@ -4,33 +4,33 @@
 
 using Markdown
 
-md"Import Librairies"
+md"Import librairies"
 using CSV, DataFrames
 using MLJ
 
-md"Read Dataset => `df`"
+md"Read dataset and assign it `df`"
 df = CSV.read("../Datasets/Social_Network_Ads.csv", DataFrame)
 
-md"Unpack Data"
+md"Unpack data"
 features, target = unpack(df,
                           ==(:EstimatedSalary),
                           ==(:Purchased);
                           :EstimatedSalary => Continuous,
                           :Purchased => Multiclass)
 
-md"Scatter Plot"
+md"Scatter plot"
 scatter(features, target; group=target, legend=false)
 
-md"Convert Data"
+md"Convert data"
 x = Tables.table(features);
 y = target;
 
-md"Bind An Instance `dtc_` Model To Training Data"
+md"Bind an instance `dtc_` model to training data"
 DTC = @load DecisionTreeClassifier pkg=DecisionTree
 dtc_ = DTC(max_depth=5, min_samples_split=3)
 dtc = machine(dtc_, x, y) |> fit!
 
 md"You may want to see [DecisionTree.jl](https://github.com/bensadeghi/DecisionTree.jl) and the unwrapped model type [`MLJDecisionTreeInterface.DecisionTree.DecisionTreeClassifier`](@ref)."
 
-md"Evaluate Model"
+md"Evaluate model"
 evaluate!(dtc)
