e182f75fe486438cf84f3c88e83593b690094974
a-mhamdi <a_mhamdi@outlook.com>
2023-04-10 11:39:54 +0100

    k-nearest neighbors

--- Codes/Julia/Part-2/knn.jl
+++ Codes/Julia/Part-2/knn.jl
@@ -4,30 +4,30 @@
 
 using Markdown
 
-md"Import Librairies"
+md"Import librairies"
 using CSV, DataFrames
 using Plots; # unicodeplots()
 using MLJ
 
-md"Read Dataset => `df`"
+md"Read dataset -> `df`"
 df = CSV.read("../Datasets/Social_Network_Ads.csv", DataFrame)
 
-md"Unpack Data"
+md"Unpack data"
 features, target = unpack(df,
                             ==(:EstimatedSalary),
                             ==(:Purchased);
                             :EstimatedSalary => Continuous,
                             :Purchased => Multiclass)
 
-md"Scatter Plot"
+md"Scatter plot"
 scatter(features, target; group=target, legend=false)
 
-md"Split The Data Into Train & Test Sets"
+md"Split the data into train & test sets"
 train, test = partition(eachindex(target), 0.8, rng=123)
 xtrain, xtest = table(features[train, :]), table(features[test, :])
 ytrain, ytest = target[train], target[test]
 
-md"Find Nearest Neighbors"
+md"Find nearest neighbors"
 KNN = @load KNNClassifier pkg=NearestNeighborModels
 knn_ = KNN(K=3)
 knn = machine(knn_, xtrain, ytrain) |> fit!
@@ -37,7 +37,7 @@ md"You may want to see [NearestNeighborModels.jl](https://github.com/JuliaAI/Nea
 md"Make predictions on `xtest`"
 yhat = predict_mode(knn, xtest)
 
-md"Confusion Matrix"
+md"Confusion matrix"
 confusion_matrix(yhat, ytest)
 
 md"Evaluation the model's performances"
