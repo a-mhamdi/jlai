6c172aff8696cbb3aae45005ff8c540510674612
a-mhamdi <a_mhamdi@outlook.com>
2024-12-03 23:23:21 +0100

    update Docker part-3

---
+++ Docker/script.sh
@@ -0,0 +1,9 @@
+DIRS=$(ls -la | grep '^d' | awk '{print $9}' | grep -v '^\.')
+
+for dir in $DIRS; do
+	cd ${WORKING_DIR}$dir
+	# sed -i '/^Image/d' Project.toml 
+	sed '/^Image/d' Project.toml > tmp && cat tmp > Project.toml && rm tmp
+	# awk '!/^Image/' Project.toml > ${WORKING_DIR}/temp && mv ${WORKING_DIR}/temp ${WORKING_DIR}/Project.toml
+	julia -e 'import Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile(); Pkg.resolve()'
+done
