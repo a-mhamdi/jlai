5deb320bac351fca3d4e80db24ac4e3bdc1cafed
a-mhamdi <a_mhamdi@outlook.com>
2024-12-08 20:07:23 +0100

    upd docker config files

--- Codes/Julia/Part-3/script.sh
+++ Codes/Julia/Part-3/script.sh
@@ -2,8 +2,9 @@ DIRS=$(ls -la | grep '^d' | awk '{print $9}' | grep -v '^\.')
 
 for dir in $DIRS; do
 	cd ${WORKING_DIR}$dir
+	rm Manifest.toml
+	sed '/^ImageShow/d' Project.toml > tmp && cat tmp > Project.toml && rm tmp
 	# sed -i '/^Image/d' Project.toml 
-	sed '/^Image/d' Project.toml > tmp && cat tmp > Project.toml && rm tmp
 	# awk '!/^Image/' Project.toml > ${WORKING_DIR}/temp && mv ${WORKING_DIR}/temp ${WORKING_DIR}/Project.toml
-	julia -e 'import Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.precompile(); Pkg.resolve()'
+	julia -e 'import Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.precompile();'
 done
